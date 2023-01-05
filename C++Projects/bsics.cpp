598d862f56e103b443b94ff2c7bf35af5609d112
Olan <olanhealy12@gmail.com>
2023-01-05 14:54:56 +0000

    Looking at basics of C++

---
+++ C++Projects/bsics.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main() {
+    //This is a comment
+    /*
+    This is
+    multi line
+    comment
+    */
+   /* 
+   ** Begging's
+    std::cout << "I still love :(" << '\n';
+    std::cout << "its not good" << '\n';
+    */
+/*
+** declaring and assigning ints
+   int x;
+   x = 8;
+   int y = 10;
+   std::cout << x << '\n';
+   std::cout << y << '\n';
+   std::cout << x + y << '\n';
+   */
+/*
+* double *
+  double price = 89.99;
+  double QCA = 3.41;
+  std::cout << price << '\n';
+  std::cout << QCA << '\n';
+   */
+/*
+*char 
+  char grade = 'A';
+  char initial = 'B';
+  char currency = '$';
+
+  std::cout << currency; 
+  */
+ /*
+ * boolean (true/false)
+ bool student = true;
+ bool power = false;
+*/
+
+// strings
+std::string name = "Arnold";
+std::string player = "Nunez";
+std::string player1 = "Poonez";
+std::cout << "Hello " << name << '\n';
+std::cout << "I know you love " << player << '\n';
+std::cout << "But he should be called " << player1 << '\n';
+
+
+     return 0;
+}
