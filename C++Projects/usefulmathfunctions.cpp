464f2fc9ce1d0b690b9befbbe1573e4af2a53e44
Olan <olanhealy12@gmail.com>
2023-01-10 16:50:59 +0000

    Useful math functions

---
+++ C++Projects/usefulmathfunctions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+int main() {
+double x = 3.3;
+double y =4;
+double z;
+
+//z = std::max(x, y);
+//z = std::min(x, y);
+//z = pow(2, 3);
+//z = sqrt(25);
+//z = abs(-90);
+//z = round(x);
+z = ceil(x);
+std::cout << z;
+
+
+
+
+    return 0;
+}
